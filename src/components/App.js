87584c92ae019fe7115e88c6a081f99dfc220549
Vivek Tiwari <79350756+tivivek@users.noreply.github.com>
2023-01-19 00:37:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
   const handleInput = (event) =>{
-   // use console.log
+    // use console.log
+    event.preventDefault()
+    console.log(`Input in #${event.target.id} is ${event.target.value}`);
   }
 
   // do not change id of input elements
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
-
+      <input id="text-input" type={'text'} onChange={handleInput}/>
       <br/>
       <br/>
-
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
+      <input id="num-input"  type={'number'} onChange={handleInput}/>
       <br/>
     </div>
   )
